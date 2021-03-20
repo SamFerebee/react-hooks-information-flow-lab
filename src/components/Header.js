e9832bead181ec426623a80d2bff23f1d63f15ae
Sam Ferebee <sam.ferebee@gmail.com>
2021-03-19 22:46:51 -0400

    Done.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = ({darkMode, onDarkModeClick}) => {
+
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+                {darkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+export default Header
